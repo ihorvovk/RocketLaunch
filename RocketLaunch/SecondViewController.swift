cb6dfc1d8219387bc468c18ad4c427bc490b8044
Ihor Vovk <i.vovk@wnb.com.ua>
2019-05-01 16:03:13 +0300

    Initial setup. Add dependencies.

---
+++ RocketLaunch/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RocketLaunch
+//
+//  Created by Ihor Vovk on 5/1/19.
+//  Copyright © 2019 Ihor vovk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
